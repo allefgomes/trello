8bcba7d58ae6ff17e6bfd8db2700f6568cf9da85
Allef Gomes <allefgalmeida@gmail.com>
2022-11-19 14:59:47 -0300

    build: add sortablejs to move cards between lists

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,8 +25,17 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+import Drag from './dragHook';
+
+let Hooks = { };
+
+Hooks.Drag = Drag;
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  hooks: Hooks,
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
